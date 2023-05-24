9aaebb942eb5f0235ead4ebcef96de5ba3bb6309
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2023-05-24 12:44:13 -0400

    styles changes

--- src/Projectos.jsx
+++ src/Projectos.jsx
@@ -9,12 +9,12 @@ function Projectos() {
     <section className="Projectos" id="projects">
         <h2>PROJECTS</h2>
         <div className="Projectos-container">
-            <Container>
+        <Container>
             <Row>
                 <Col className="Projecto" lg={4} md={12}>
                     <img src={projectOne} alt="ecommerce app" />
                     <h4>E-commerce</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, harum.</p>
+                    <p>Project was made using React, React-Router, Bootstrap, Material-UI Context API, Firebase & Local Storage.</p>
                     <Button variant="outline-dark">
                         <a href="https://jocular-crumble-48888e.netlify.app" target="_blank" rel="noopener noreferrer">
                         Live Demo
@@ -23,8 +23,8 @@ function Projectos() {
                 </Col>
                 <Col className="Projecto" lg={4} md={12}>
                     <img src={projectTwo} alt="Private routes app" />
-                    <h4>JWT & Cookies</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, harum.</p>
+                    <h4>Private Routes-JWT</h4>
+                    <p>Project was made using React, React-Router, JWT, Monggose, MongoDB, Bcrypt, Express, Bootstrap & CSS. Axios and Fetch to get users and products data.</p>
                     <Button variant="outline-dark">
                         <a href="https://super-crostata-0b6c03.netlify.app" target="_blank" rel="noopener noreferrer">
                             Live Demo
@@ -34,7 +34,7 @@ function Projectos() {
                 <Col className="Projecto" lg={4} md={12}>
                     <img src={projectThree} alt="CRUD app" />
                     <h4>CRUD App</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, harum.</p>
+                    <p>Project was made using Nodejs, Mongoose, MongoDB, Express, Bootstrap. Model-view-controller software design pattern was use.</p>
                     <Button variant="outline-dark">
                         <a href="https://nodejs-mongodb-tasks-crud-production.up.railway.app/" target="_blank" rel="noopener noreferrer">
                             Live Demo
@@ -42,7 +42,7 @@ function Projectos() {
                     </Button>
                 </Col>
             </Row>
-            </Container>
+        </Container>
         </div>
         <Button className="m-4" variant="info">
             More Projects

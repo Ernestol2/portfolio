ec3acdcead50f22a8b4239832b1e070484a88d50
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2023-07-06 15:02:28 -0400

    more projects update1

--- src/MoreProjects.jsx
+++ src/MoreProjects.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import calculatorReact from './images/calculator-react.png'
@@ -9,6 +10,12 @@ import ecommerce from './images/sushi-store.png'
 
  
 export default function MoreProjects() {
+
+  useEffect(() => {
+    window.scrollTo(0 , 0);
+  }, [])
+
+
   return (
     <div className='more'>
       <h2>OTROS PROYECTOS</h2>
@@ -19,81 +26,89 @@ export default function MoreProjects() {
                 <Card style={{ width: '23rem' }}>
                   <Card.Img variant="top" src={colorFlipper} />
                   <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>Color Flipper</Card.Title>
                     <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the cards content.
+                      Proyecto para generar colores hex aleatorios usando HTML, CSS y javaScript.
                     </Card.Text>
-                    <Button variant="outline-dark">Go somewhere</Button>
+                    <a href="https://hex-color-generator-el.netlify.app/" target='_blank' rel='noreferrer'>
+                    <Button variant="outline-light">Visitar</Button>
+                    </a>
                   </Card.Body>
                 </Card>
               </Col>
               <Col xs={12} md={6}>
-              <Card style={{ width: '24.5rem' }}>
+              <Card style={{ width: '23rem' }}>
                   <Card.Img variant="top" src={drumMachine} />
                   <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>Drum Machine</Card.Title>
                     <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the cards content.
+                      Proyecto para recrear una bateria. Una pequeña pantalla muestra el sonido reproducido.
                     </Card.Text>
-                    <Button variant="outline-dark">Go somewhere</Button>
+                    <a href="https://super-torte-46389c.netlify.app/" target='_blank' rel='noreferrer'>
+
+                      <Button variant="outline-light">Visitar</Button>
+                    </a>
                   </Card.Body>
                 </Card>
               </Col>
             </Row>
             <Row>
               <Col xs={12} md={6}>
-                <Card style={{ width: '24.5rem' }}>
+                <Card style={{ width: '23rem' }}>
                   <Card.Img variant="top" src={ecommerce} />
                   <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>E-commerce</Card.Title>
                     <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the cards content.
+                      Proyecto creado con React. Usando un context API para manejar el estado global de la aplicacion.
                     </Card.Text>
-                    <Button variant="outline-dark">Go somewhere</Button>
+                    <a href="https://jocular-crumble-48888e.netlify.app/" target='_blank' rel='noreferrer'>
+                      <Button variant="outline-light">Visitar</Button>
+                    </a>
                   </Card.Body>
                 </Card>
               </Col>
               <Col xs={12} md={6}>
-              <Card style={{ width: '24.5rem' }}>
+              <Card style={{ width: '23rem' }}>
                   <Card.Img variant="top" src={calculatorReact} />
                   <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>javaScript Calculator</Card.Title>
                     <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the cards content.
+                      Proyecto creado con React y bootstrap para los estilos. Una simple pero muy eficiente calculadora.
                     </Card.Text>
-                    <Button variant="outline-dark">Go somewhere</Button>
+                    <a href="https://new-calculator-elopez.netlify.app/" target='_blank' rel='noreferrer'>
+                      <Button variant="outline-light">Visitar</Button>
+                    </a>
                   </Card.Body>
                 </Card>
               </Col>
             </Row>
             <Row>
               <Col xs={12} md={6}>
-                <Card style={{ width: '24.5rem' }}>
+                <Card style={{ width: '23rem' }}>
                   <Card.Img variant="top" src={portafolio} />
                   <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>Portafolio</Card.Title>
                     <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the cards content.
+                      Vite.js, React-router, React-bootstrap, React-lazy-load son algunos de los paquetes que se usaron para su construccion.
                     </Card.Text>
-                    <Button variant="outline-dark">Go somewhere</Button>
+                    <a href="https://ernesto-lopez-portfolio.netlify.app/" target='_blank' rel='noreferrer'>
+                      <Button variant="outline-light">Visitar</Button>
+                    </a>
                   </Card.Body>
                 </Card>
               </Col>
               <Col xs={12} md={6}>
-              <Card style={{ width: '24.5rem' }}>
+              <Card style={{ width: '23rem' }}>
                   <Card.Img variant="top" src={pomodoroClock} />
                   <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
+                    <Card.Title>Pomodoro Clock</Card.Title>
                     <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the cards content.
+                      Reloj de cuenta regresiva desarrollado con javaScript.
+                      Simula un reloj para descansar despues de un periodo de trabajo determinado.  
                     </Card.Text>
-                    <Button variant="outline-dark">Go somewhere</Button>
+                    <a href="https://bucolic-mooncake-46cb13.netlify.app/" target='_blank' rel='noreferrer'>
+                    <Button variant="outline-light">Visitar</Button>
+                    </a>
                   </Card.Body>
                 </Card>
               </Col>
@@ -101,7 +116,7 @@ export default function MoreProjects() {
           </Container>
         </div>
         <Link to='/' relative='path'>
-          <Button className='m-4' variant='dark'>
+          <Button className='m-4' variant='outline-dark'>
             Volver
           </Button>
         </Link>
